4d401fca706c413268f4967df472a0b6cd274756
Kirill <bloodwingest@gmail.com>
2022-10-25 01:21:01 +0300

    fix: fixed empty tile

--- gem-puzzle/src/index.js
+++ gem-puzzle/src/index.js
@@ -10,96 +10,16 @@ let count = 0;
 let previousSize;
 let currentSize = 4;
 
-let audio = new Audio('audio/click.wav');
-let soundOn = true;
+/* timer */
 
+let startTime = 0;
+let elapsedTime = 0;
+let timeInterval;
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
 
-class Tile {
-    constructor(position, value, isClickable){
-        this.position = position;
-        this.value = value;
-        this.isClickable = isClickable;
-    }
-}
-
-function initializeLayout(size){
-    const body = document.querySelector('body');
-    body.innerHTML = puzzleWrapper;
-    
-    const grid = document.querySelector('.game-grid');
-    
-    const newGame = document.querySelector('.shuffle')
-    const check = document.querySelector('.check')
-    const audioTumbler = document.querySelector('.sound')
-
-    const size3 = document.querySelector('.size3')
-    const size4 = document.querySelector('.size4')
-    const size5 = document.querySelector('.size5')
-    const size6 = document.querySelector('.size6')
-    const size7 = document.querySelector('.size7')
-    const size8 = document.querySelector('.size8')
-
-    grid.classList.remove(`size-${previousSize}`)
-    grid.classList.add(`size-${size}`)
-
-    let newEmptyTile = document.createElement('div');
-    newEmptyTile.classList.add('game-tile', 'empty-tile')
-    newEmptyTile.numValue = 0;
-    newEmptyTile.textContent = 0;
-    grid.appendChild(newEmptyTile)
-
-    for (let i = 1; i < gridSize; i++) {
-        let newTile = document.createElement('div')
-        newTile.classList.add('game-tile')
-        newTile.numValue = i;
-        newTile.textContent = i
-        grid.appendChild(newTile)
-    }
-    
-    const tile = document.querySelector('.game-tile');
-    const emptyTile = document.querySelector('.empty-tile')
-
-    newGame.addEventListener('click', shuffleNumbers)
-    newGame.addEventListener("click", () => {
-        clearInterval(timeInterval, 1000)
-        const timeDisplay = document.querySelector('.timer')
-        timeDisplay.textContent = `00:00:00`;
-        elapsedTime = 0;
-        startTime = Date.now() - elapsedTime;
-        timeInterval = setInterval(updateTime, 1000);
-    });
-
-    audioTumbler.addEventListener('click', changeSound)
-    check.addEventListener('click', checkPositions)
-    emptyTile.addEventListener('mouseover', checkPositions)
-
-    size3.addEventListener('click', ()=>{
-        changeSize(3);
-    })
-    size4.addEventListener('click', ()=>{
-        changeSize(4);
-    })
-    size5.addEventListener('click', ()=>{
-        changeSize(5);
-    })
-    size6.addEventListener('click', ()=>{
-        changeSize(6);
-    })
-    size7.addEventListener('click', ()=>{
-        changeSize(7);
-    })
-    size8.addEventListener('click', ()=>{
-        changeSize(8);
-    })
-
-    let startTime = 0;
-    let elapsedTime = 0;
-    let timeInterval;
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 0;
-
-function updateTime(){
+function updateTime() {
     elapsedTime = Date.now() - startTime;
     console.log(elapsedTime);
     seconds = Math.floor((elapsedTime / 1000) % 60);
@@ -111,26 +31,131 @@ function updateTime(){
     hours = pad(hours);
     const timeDisplay = document.querySelector('.timer')
     timeDisplay.textContent = `${hours}:${minutes}:${seconds}`;
-    function pad(timeUnit){
+    function pad(timeUnit) {
         return (("0") + timeUnit).length > 2 ? timeUnit : "0" + timeUnit;
-        
+    }
 }
-}
+
+let audio = new Audio('audio/click.wav');
+let soundOn = true;
+
+
+/* class Tile {
+    constructor(position, value, isClickable) {
+        this.position = position;
+        this.value = value;
+        this.isClickable = isClickable;
+    }
+} */
+
+function initializeLayout(size) {
+    const body = document.querySelector('body');
+    body.innerHTML = puzzleWrapper;
+
+    const grid = document.querySelector('.game-grid');
+    const newGame = document.querySelector('.shuffle')
+    const audioTumbler = document.querySelector('.sound')
+    const size3 = document.querySelector('.size3')
+    const size4 = document.querySelector('.size4')
+    const size5 = document.querySelector('.size5')
+    const size6 = document.querySelector('.size6')
+    const size7 = document.querySelector('.size7')
+    const size8 = document.querySelector('.size8')
+
+    grid.classList.remove(`size-${previousSize}`)
+    if (currentSize === 3) {
+        grid.classList.remove('size-4')
+    }
+    grid.classList.add(`size-${size}`)
+
+    let newEmptyTile = document.createElement('div');
+    newEmptyTile.classList.add('game-tile', 'empty-tile')
+    newEmptyTile.numValue = 0;
+    newEmptyTile.textContent = "";
+    grid.appendChild(newEmptyTile)
+
+    for (let i = 1; i < gridSize; i++) {
+        let newTile = document.createElement('div')
+        newTile.classList.add('game-tile')
+        newTile.numValue = i;
+        newTile.textContent = i
+        grid.appendChild(newTile)
+    }
+
+    const tile = document.querySelector('.game-tile');
+    const emptyTile = document.querySelector('.empty-tile')
+
+    /* event listeners */
+    newGame.addEventListener('click', shuffleNumbers)
+
+    audioTumbler.addEventListener('click', changeSound)
+    emptyTile.addEventListener('mouseover', checkPositions)
+
+    size3.addEventListener('click', () => {
+        changeSize(3);
+    })
+    size4.addEventListener('click', () => {
+        changeSize(4);
+    })
+    size5.addEventListener('click', () => {
+        changeSize(5);
+    })
+    size6.addEventListener('click', () => {
+        changeSize(6);
+    })
+    size7.addEventListener('click', () => {
+        changeSize(7);
+    })
+    size8.addEventListener('click', () => {
+        changeSize(8);
+    })
+    /* timer */
+
+
+
+    newGame.addEventListener("click", () => {
+        clearInterval(timeInterval, 1000)
+        const timeDisplay = document.querySelector('.timer')
+        timeDisplay.textContent = `00:00:00`;
+        elapsedTime = 0;
+        startTime = Date.now() - elapsedTime;
+        timeInterval = setInterval(updateTime, 1000);
+    });
+    window.addEventListener('load', () => {
+        clearInterval(timeInterval, 1000)
+        const timeDisplay = document.querySelector('.timer')
+        timeDisplay.textContent = `00:00:00`;
+        elapsedTime = 0;
+        startTime = Date.now() - elapsedTime;
+        timeInterval = setInterval(updateTime, 1000);
+    });
+    /* sound fix */
+    if (soundOn === false) {
+        audioTumbler.textContent = "Sound: Off"
+    } else {
+        audioTumbler.textContent = "Sound: On"
+    }
 
 }
 
-function shuffleNumbers(){
+function shuffleNumbers() {
     updateCount(0, true)
     removeListeners()
     const emptyTile = document.querySelector('.empty-tile')
     emptyTile.addEventListener('mouseover', checkPositions)
-
     const tileNumbers = document.querySelectorAll('.game-tile');
+
+    clearInterval(timeInterval, 1000)
+    const timeDisplay = document.querySelector('.timer')
+    timeDisplay.textContent = `00:00:00`;
+    elapsedTime = 0;
+    startTime = Date.now() - elapsedTime;
+    timeInterval = setInterval(updateTime, 1000);
 
     let randomNumbers = []
     for (let i = 0; i < tileNumbers.length; i++) {
         let randomNumber = Math.round(Math.random() * (tileNumbers.length - 1) + 1);
-        while(randomNumbers.includes(randomNumber)){
+        while (randomNumbers.includes(randomNumber)) {
             randomNumber = Math.round(Math.random() * (tileNumbers.length - 1) + 1);
         }
         randomNumbers.push(randomNumber)
@@ -139,11 +164,11 @@ function shuffleNumbers(){
     for (let i = 0; i < tileNumbers.length; i++) {
         tileNumbers[i].parentNode.insertBefore(tileNumbers[i], tileNumbers[randomNumbers[i]])
     }
-    
+
     checkPositions();
 }
 
-function checkPositions(){
+function checkPositions() {
     let tileNumber = []
     const tiles = document.querySelectorAll('.game-tile');
 
@@ -152,66 +177,70 @@ function checkPositions(){
     }
 
     for (let i = 0; i < tiles.length; i++) {
-        if(tiles[i].textContent == 0){
-                                            //just i because we count table from 0
-                if(tiles[i-1] !== undefined && (i) % width > 0 ){//left from zero
-                    tiles[i-1].classList.add('red')
-                    setTimeout(() => {
-                        tiles[i-1].classList.remove('red')}
+        if (tiles[i].textContent == 0) {
+            //just i because we count table from 0
+            if (tiles[i - 1] !== undefined && (i) % width > 0) {//left from zero
+                tiles[i - 1].classList.add('red')
+                setTimeout(() => {
+                    tiles[i - 1].classList.remove('red')
+                }
                     , 150)
-                    tiles[i-1].addEventListener('click', moveRight)
-                } else {
-                    console.log('ашипка влево');
-                }
+                tiles[i - 1].addEventListener('click', moveRight)
+            } else {
+                console.log('ашипка влево');
+            }
 
-                if(tiles[i+1] !== undefined && (i+1) % width > 0 ){//right from zero
-                    tiles[i+1].classList.add('red')
-                    setTimeout(() => {
-                        tiles[i+1].classList.remove('red')}
+            if (tiles[i + 1] !== undefined && (i + 1) % width > 0) {//right from zero
+                tiles[i + 1].classList.add('red')
+                setTimeout(() => {
+                    tiles[i + 1].classList.remove('red')
+                }
                     , 150)
-                    tiles[i+1].addEventListener('click', moveLeft)
-                } else {
-                    console.log('ашипка вправо');
-                }
+                tiles[i + 1].addEventListener('click', moveLeft)
+            } else {
+                console.log('ашипка вправо');
+            }
 
-                if(tiles[i-width] !== undefined){//up from zero
-                    tiles[i-width].classList.add('red')
-                    setTimeout(() => {
-                        tiles[i-width].classList.remove('red')}
-                    ,150)
-                    tiles[i-width].addEventListener('click', moveDown)
-                } else {
-                    console.log(tiles[i-1]);
-                    console.log('ашипка вверх');
+            if (tiles[i - width] !== undefined) {//up from zero
+                tiles[i - width].classList.add('red')
+                setTimeout(() => {
+                    tiles[i - width].classList.remove('red')
                 }
-                if(tiles[i+width] !== undefined){//down from zero
-                    tiles[i+width].classList.add('red')
-                    setTimeout(() => {
-                        tiles[i+width].classList.remove('red')}
-                    ,150)
-                    tiles[i+width].addEventListener('click', moveUp)
-                } else {
-                    console.log('ашипка вниз');
+                    , 150)
+                tiles[i - width].addEventListener('click', moveDown)
+            } else {
+                console.log(tiles[i - 1]);
+                console.log('ашипка вверх');
+            }
+            if (tiles[i + width] !== undefined) {//down from zero
+                tiles[i + width].classList.add('red')
+                setTimeout(() => {
+                    tiles[i + width].classList.remove('red')
                 }
+                    , 150)
+                tiles[i + width].addEventListener('click', moveUp)
+            } else {
+                console.log('ашипка вниз');
             }
         }
-    
+    }
+
 }
 
-function updateCount(add=1, reset){
+function updateCount(add = 1, reset) {
     const counter = document.querySelector('.count')
-    count+=add;
-    if(reset){
+    count += add;
+    if (reset) {
         count = 0;
     }
     counter.textContent = `Count: ${count}`;
 }
 /*move functions*/
-function moveRight(event){
+function moveRight(event) {
     const tiles = document.querySelectorAll('.game-tile');
     for (let i = 0; i < tiles.length; i++) {
-        if(tiles[i] === event.target){
-            tiles[i].parentNode.insertBefore(tiles[i], tiles[i+2])
+        if (tiles[i] === event.target) {
+            tiles[i].parentNode.insertBefore(tiles[i], tiles[i + 2])
             removeListeners()
             const emptyTile = document.querySelector('.empty-tile')
             emptyTile.addEventListener('mouseover', checkPositions)
@@ -220,11 +249,11 @@ function moveRight(event){
     updateCount();
     playSound()
 }
-function moveLeft(event){
+function moveLeft(event) {
     const tiles = document.querySelectorAll('.game-tile');
     for (let i = 0; i < tiles.length; i++) {
-        if(tiles[i] === event.target){
-            tiles[i].parentNode.insertBefore(tiles[i], tiles[i-1])
+        if (tiles[i] === event.target) {
+            tiles[i].parentNode.insertBefore(tiles[i], tiles[i - 1])
             event.target.removeEventListener('click', moveLeft)
             removeListeners()
             const emptyTile = document.querySelector('.empty-tile')
@@ -234,12 +263,12 @@ function moveLeft(event){
     updateCount();
     playSound()
 }
-function moveDown(event){
+function moveDown(event) {
     const tiles = document.querySelectorAll('.game-tile');
     for (let i = 0; i < tiles.length; i++) {
-        if(tiles[i] === event.target){
-            tiles[i].parentNode.insertBefore(tiles[i], tiles[i+5])
-            tiles[i+4].parentNode.insertBefore(tiles[i+4], tiles[i+1])
+        if (tiles[i] === event.target) {
+            tiles[i].parentNode.insertBefore(tiles[i], tiles[i + width + 1])
+            tiles[i + width].parentNode.insertBefore(tiles[i + width], tiles[i + 1])
             removeListeners()
             const emptyTile = document.querySelector('.empty-tile')
             emptyTile.addEventListener('mouseover', checkPositions)
@@ -248,12 +277,12 @@ function moveDown(event){
     updateCount();
     playSound()
 }
-function moveUp(event){
+function moveUp(event) {
     const tiles = document.querySelectorAll('.game-tile');
     for (let i = 0; i < tiles.length; i++) {
-        if(tiles[i] === event.target){
-            tiles[i].parentNode.insertBefore(tiles[i], tiles[i-4])
-            tiles[i-4].parentNode.insertBefore(tiles[i-4], tiles[i+1])
+        if (tiles[i] === event.target) {
+            tiles[i].parentNode.insertBefore(tiles[i], tiles[i - width])
+            tiles[i - width].parentNode.insertBefore(tiles[i - width], tiles[i + 1])
             removeListeners()
             const emptyTile = document.querySelector('.empty-tile')
             emptyTile.addEventListener('mouseover', checkPositions)
@@ -263,10 +292,10 @@ function moveUp(event){
     playSound()
 }
 
-function removeListeners(){
+function removeListeners() {
     const tiles = document.querySelectorAll('.game-tile')
     for (let i = 0; i < tiles.length; i++) {
-        tiles[i].parentNode.replaceChild(tiles[i].cloneNode(true), tiles[i])        
+        tiles[i].parentNode.replaceChild(tiles[i].cloneNode(true), tiles[i])
     }
 }
 
@@ -276,25 +305,25 @@ shuffleNumbers();
 
 /*звуки*/
 
-function changeSound(){
+function changeSound() {
     const audioTumbler = document.querySelector('.sound')
     soundOn === true ? soundOn = false : soundOn = true;
-    if(soundOn === false){
+    if (soundOn === false) {
         audioTumbler.textContent = "Sound: Off"
-    } else{
+    } else {
         audioTumbler.textContent = "Sound: On"
     }
 }
 
-function playSound(){
-    if(soundOn){
+function playSound() {
+    if (soundOn) {
         audio.play()
     }
 }
 
 /*change size*/
-function changeSize(size){
-    if(size === 3){
+function changeSize(size) {
+    if (size === 3) {
         width = size;
         previousSize = currentSize;
         currentSize = size;
@@ -303,7 +332,7 @@ function changeSize(size){
         initializeLayout(size);
         shuffleNumbers();
     }
-    if(size === 4){
+    if (size === 4) {
         width = size;
         previousSize = currentSize;
         currentSize = size;
@@ -312,7 +341,7 @@ function changeSize(size){
         initializeLayout(size);
         shuffleNumbers();
     }
-    if(size === 5){
+    if (size === 5) {
         width = size;
         previousSize = currentSize;
         currentSize = size;
@@ -321,7 +350,7 @@ function changeSize(size){
         initializeLayout(size);
         shuffleNumbers();
     }
-    if(size === 6){
+    if (size === 6) {
         width = size;
         previousSize = currentSize;
         currentSize = size;
@@ -330,7 +359,7 @@ function changeSize(size){
         initializeLayout(size);
         shuffleNumbers();
     }
-    if(size === 7){
+    if (size === 7) {
         width = size;
         previousSize = currentSize;
         currentSize = size;
@@ -339,7 +368,7 @@ function changeSize(size){
         initializeLayout(size);
         shuffleNumbers();
     }
-    if(size === 8){
+    if (size === 8) {
         width = size;
         previousSize = currentSize;
         currentSize = size;
